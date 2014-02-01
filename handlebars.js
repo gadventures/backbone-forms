701185df745c8dc948a308cdaa3add40f09fe526
Bartek Ciszkowski <bart.ciszk@gmail.com>
2014-02-01 00:40:46 -0500

    Tell users of package that brfs transform is required for Handlebars sanity.

--- handlebars.js
+++ handlebars.js
@@ -1,18 +1,19 @@
+/*global __dirname*/
 var Handlebars = require('handlebars'),
     fs = require('fs');
 
 var templates = {
-    form: fs.readFileSync('./templates/form.hbs'),
-    fieldset: fs.readFileSync('./templates/fieldset.hbs'),
-    field: fs.readFileSync('./templates/field.hbs'),
-    field_errors: fs.readFileSync('./templates/errors.hbs'),
-    text: fs.readFileSync('./templates/input_text.hbs'),
-    email: fs.readFileSync('./templates/input_text.hbs'),
-    date: fs.readFileSync('./templates/input_date.hbs'),
-    select: fs.readFileSync('./templates/input_select.hbs'),
-    password: fs.readFileSync('./templates/input_text.hbs'),
-    checkbox: fs.readFileSync('./templates/input_checkbox.hbs'),
-    textarea: fs.readFileSync('./templates/textarea.hbs')
+    form: fs.readFileSync(__dirname + '/templates/form.hbs'),
+    fieldset: fs.readFileSync(__dirname + '/templates/fieldset.hbs'),
+    field: fs.readFileSync(__dirname + '/templates/field.hbs'),
+    field_errors: fs.readFileSync(__dirname + '/templates/errors.hbs'),
+    text: fs.readFileSync(__dirname + '/templates/input_text.hbs'),
+    email: fs.readFileSync(__dirname + '/templates/input_text.hbs'),
+    date: fs.readFileSync(__dirname + '/templates/input_date.hbs'),
+    select: fs.readFileSync(__dirname + '/templates/input_select.hbs'),
+    password: fs.readFileSync(__dirname + '/templates/input_text.hbs'),
+    checkbox: fs.readFileSync(__dirname + '/templates/input_checkbox.hbs'),
+    textarea: fs.readFileSync(__dirname + '/templates/textarea.hbs')
 };
 
 // Store compiled templates.
